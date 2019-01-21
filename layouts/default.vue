644c7b8acf00dae3ef553c1156608b1c11890768
jaubut <jeremie.aubut@gmail.com>
2019-01-21 10:15:20 -0500

    remove the alert

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div class="alert">
-      <small>Exceptionnellement, le rassemblement d'aujourd'hui est annulé à cause de la température. Bon dimanche à tous !</small>
-    </div>
     <header>
       <router-link to="/">
         <h1 class="logo">Sainte Scène</h1>

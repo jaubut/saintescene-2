2afb3e23068bcd370bc5201c6bc480c0e0e9b4a3
jaubut <jeremie.aubut@gmail.com>
2019-02-27 09:51:52 -0500

    remove fevrier

--- pages/evenements.vue
+++ pages/evenements.vue
@@ -3,7 +3,6 @@
     <Hero class="hero-evenement">
       <div class="text"><h1>Calendrier</h1></div> 
     </Hero>
-    <Calendrier Month="Février"></Calendrier>
     <Calendrier Month="Mars"></Calendrier>
     <Calendrier Month="Avril"></Calendrier>
     <div class="text-group">

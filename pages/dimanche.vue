9b6b72854975d8155b095853bcbe38ab92db9c97
Jeremie Aubut <jeremie.aubut@gmail.com>
2019-04-08 13:23:26 -0400

    change some photo again

--- pages/dimanche.vue
+++ pages/dimanche.vue
@@ -26,7 +26,7 @@
     <div class="bloc span-2 row">
       <div class="texte center">
         <img class="mini-scene" src="../assets/dimanche/mini-scene-logo.png" alt="">
-        </br>
+        <br>
         <p>Pour les enfants<br>de 3 à 5 ans, un temps <br>de louange est dédié <br>pour eux à l'étage.</p>
       </div>
       <img src="../assets/dimanche/mini-scene.jpg" alt="">

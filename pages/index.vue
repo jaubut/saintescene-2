cbbc11887b45459ede2f242fc65e853b36c5c4fd
jaubut <jeremie.aubut@gmail.com>
2019-04-22 14:12:44 -0400

    max 20 photos in the carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="Index">
     <Carousel :per-page="1" :navigate-to="0" :autoplay=true :loop=true :paginationEnabled=false>
-      <Slide v-for="photo in photos" :key="photo.fields.title">
+      <Slide v-for="photo in photos.slice(0, 20)" :key="photo.fields.title">
         <Hero class="accueil" :style="{'background-image': 'url(' + photo.fields.file.url + '?w=1200&h=1200' + ')'}">
           <div class="text">
             <img src="~/assets/logo-white.svg" height="250px" class="logo-hero" alt="">
